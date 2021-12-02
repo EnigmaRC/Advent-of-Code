a7384357747f59e364c12313284e1cc5de16640a
Olivier Vermeulen <oliviervermeulen35@gmail.com>
2021-12-02 22:38:00 +0100

    Solved Day 1 of 2020

--- 2020/Day 1/Program.cs
+++ 2020/Day 1/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World of 2020!");
-Console.ReadKey();
+﻿using System;
+using System.Text;
+
+class Day1
+{
+    public static void Main()
+    {
+        string path = @"C:\Users\Olivier PC\source\repos\Advent Of Code\2020\Day 1\input.txt";
+
+        try
+        {
+            var list = File.ReadLines(path).Select(line => int.Parse(line)).ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 1; j < list.Count - 1; j++)
+                {
+                    for (int k = 2; k < list.Count - 2; k++)
+                    {
+                        if (list[i] + list[j] + list[k] == 2020)
+                        {
+                            Console.WriteLine(list[i] * list[j] * list[k]);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("The file could not be read: ");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
